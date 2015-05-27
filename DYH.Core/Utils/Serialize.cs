5c153d742bd22a33b43a7d75fc41433cbbe1a3bb
dennisdyh <dennisdyh@126.com>
2015-05-27 11:12:51 +0800

    add some files

---
+++ DYH.Core/Utils/Serialize.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DYH.Core.Utils
+{
+    public class Serialize
+    {
+        public static T Dereference<T>(T source)
+        {
+            var formatter = new BinaryFormatter(null, new StreamingContext(StreamingContextStates.Clone));
+            object obj = null;
+            using (var stream = new MemoryStream())
+            {
+                formatter.Serialize(stream, source);
+                stream.Position = 0;
+                obj = formatter.Deserialize(stream);
+                stream.Flush();
+                stream.Close();
+            }
+
+            var result = (T)Convert.ChangeType(obj, source.GetType());
+
+            return result;
+        }
+    }
+}
